82b83e2a5d736151b18109abc644050be60565a3
15898596626 <2102512894@qq.com>
2025-06-07 13:27:39 +0800

    feat(rl): 完善DQN智能体

--- src/rl/dqn_agent.py
+++ src/rl/dqn_agent.py
@@ -17,4 +17,19 @@ class ReplayBuffer:
     def __init__(self, capacity=10000):
         self.buffer = []
         self.capacity = capacity
+        # 在dqn_agent.py中添加
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+        self.q_network = DQN(state_size, action_size)
+        self.target_network = DQN(state_size, action_size)
+        self.optimizer = torch.optim.Adam(self.q_network.parameters())
+        
+    def update_target(self):
+        self.target_network.load_state_dict(self.q_network.state_dict())
+        # 在test_dqn.py中添加
+def test_target_update(self):
+    agent = DQNAgent(4, 2)
+    old_weights = agent.target_network.fc1.weight.clone()
+    agent.update_target()
+    self.assertFalse(torch.equal(old_weights, agent.target_network.fc1.weight))
         
